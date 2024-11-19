b6ab1f08c65e959c30f9cf9d02c46c8e97a15daa
Zane <zmatarieh@bsd220.org>
2024-11-19 10:02:12 -0600

    Initial Commit

---
+++ DashZane (Final Project)/DashZane__Final_Project_App.swift
@@ -0,0 +1,17 @@
+//
+//  DashZane__Final_Project_App.swift
+//  DashZane (Final Project)
+//
+//  Created by Zane Matarieh on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct DashZane__Final_Project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DashZane (Final Project)Tests/DashZane__Final_Project_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  DashZane__Final_Project_Tests.swift
+//  DashZane (Final Project)Tests
+//
+//  Created by Zane Matarieh on 11/19/24.
+//
+
+import Testing
+@testable import DashZane__Final_Project_
+
+struct DashZane__Final_Project_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
